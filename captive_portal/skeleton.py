ae39db2c8b058167a9f5bc0fae1fdea225dec82d
Anish Mangal <anishmg@umich.edu>
2018-07-03 16:12:15 +0530

    Update skeleton

--- captive_portal/skeleton.py
+++ captive_portal/skeleton.py
@@ -3,10 +3,10 @@
 """
 This is a skeleton file that can serve as a starting point for a Python
 console script. To run this script uncomment the following line in the
-entry_points section in setup.cfg:
+entry_points section in setup.py:
 
-    console_scripts =
-     fibonacci = captive_portal.skeleton:run
+    [console_scripts]
+    fibonacci = captive_portal.skeleton:run
 
 Then run `python setup.py install` which will install the command `fibonacci`
 inside your current environment.
@@ -20,8 +20,18 @@ from __future__ import division, print_function, absolute_import
 import argparse
 import sys
 import logging
+import subprocess
+import BaseHTTPServer
+import cgi
 
 from captive_portal import __version__
+
+# These variables are used as settings
+PORT       = 9090         # the port in which the captive portal web server listens 
+IFACE      = "br0"      # the interface that captive portal protects
+IP_ADDRESS = "172.18.96.1" # the ip address of the captive portal (it can be the IP of IFACE) 
+USERNAME   = "Admin"
+PASSWORD   = "g0adm1n"
 
 __author__ = "Anish Mangal"
 __copyright__ = "Anish Mangal"
@@ -30,21 +40,117 @@ __license__ = "gpl3"
 _logger = logging.getLogger(__name__)
 
 
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
+'''
+'''
+class CaptivePortal(BaseHTTPServer.BaseHTTPRequestHandler):
+    """This it the http server used by the the captive portal
+    
     """
-    assert n > 0
-    a, b = 1, 1
-    for i in range(n-1):
-        a, b = b, a+b
-    return a
+   
 
+    """Initialize the captive portal html pages
+    
+        Args:
+          port (int): The port on which the portal is active
+          ip_address (istr): The ip address on which the portal is active
+          username: Authentication username
+          password: Authentication password
+    """
+    def __init__(self, port=PORT, ip_address=IP_ADDRESS, username=USERNAME, password=PASSWORD): 
+        #this is the index of the captive portal
+        #it simply redirects the user to the to login page
+        html_redirect = """
+        <html>
+        <head>
+            <meta http-equiv="refresh" content="0; url=http://%s:%s/login" />
+        </head>
+        <body>
+            <b>Redirecting to login page</b>
+        </body>
+        </html>
+        """%(ip_address, port)
+        
+        #the login page
+        html_login = """
+        <html>
+        <body>
+            <b>Login Form</b>
+            <form method="POST" action="do_login">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Submit">
+            </form>
+        </body>
+        </html>
+        """
+    
+    '''
+    if the user requests the login page show it, else
+    use the redirect page
+    '''
+    def do_GET(self):
+        path = self.path
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        if path == "/login":
+            self.wfile.write(self.html_login)
+        else:
+            self.wfile.write(self.html_redirect)
+    '''
+    this is called when the user submits the login form
+    '''
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        form = cgi.FieldStorage(
+            fp=self.rfile, 
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+        submitted_username = form.getvalue("username")
+        submitted_password = form.getvalue("password")
+        #dummy security check
+        if submitted_username == username and submitted_password == password:
+            #authorized user
+            remote_IP = self.client_address[0]
+            print("New authorization from %s" % remote_IP)
+            print("Updating IP tables")
+            subprocess.call(["iptables","-t", "nat", "-I", "PREROUTING","1", "-s", remote_IP, "-j" ,"ACCEPT"])
+            subprocess.call(["iptables", "-I", "FORWARD", "-s", remote_IP, "-j" ,"ACCEPT"])
+            self.wfile.write("You are now authorized. Navigate to any URL")
+        else:
+            #show the login form
+            self.wfile.write(self.html_login)
+
+def setup_initial_iptables(port=PORT, iface=IFACE, ip_address=IP_ADDRESS):
+    print("*********************************************")
+    print("* Note, if there are already iptables rules *")
+    print("* this script may not work. Flush iptables  *")
+    print("* at your own riks using iptables -F        *")
+    print("*********************************************")
+    print("Updating iptables")
+    print(".. Allow TCP DNS")
+    subprocess.call(["iptables", "-A", "FORWARD", "-i", iface, "-p", "tcp", "--dport", "53", "-j" ,"ACCEPT"])
+    print(".. Allow UDP DNS")
+    subprocess.call(["iptables", "-A", "FORWARD", "-i", iface, "-p", "udp", "--dport", "53", "-j" ,"ACCEPT"])
+    print(".. Allow traffic to captive portal")
+    subprocess.call(["iptables", "-A", "FORWARD", "-i", iface, "-p", "tcp", "--dport", str(port),"-d", ip_address, "-j" ,"ACCEPT"])
+    print(".. Block all other traffic")
+    subprocess.call(["iptables", "-A", "FORWARD", "-i", iface, "-j" ,"DROP"])
+    print("Redirecting HTTP traffic to captive portal")
+    subprocess.call(["iptables", "-t", "nat", "-A", "PREROUTING", "-i", iface, "-p", "tcp", "--dport", "80", "-j" ,"DNAT", "--to-destination", ip_address+":"+str(port)])
+
+def start_captive_server(port=PORT, iface=IFACE, ip_address=IP_ADDRESS, username=USERNAME, password=PASSWORD):
+    print "Starting web server"
+    httpd = BaseHTTPServer.HTTPServer(('', port), CaptivePortal)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
 
 def parse_args(args):
     """Parse command line parameters
@@ -103,7 +209,9 @@ def main(args):
     args = parse_args(args)
     setup_logging(args.loglevel)
     _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
+    setup_initial_iptables()
+    start_captive_server()
+    #print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
     _logger.info("Script ends here")
 
 
